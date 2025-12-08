483e422aa47a623266b3e53327e1ec2e184156e7
teguhsakura <teguh.sakura@yahoo.com>
2025-12-08 22:25:35 +0700

    Add files via upload
    tambahan file untuk pwa

---
+++ sw.js
@@ -0,0 +1,40 @@
+const CACHE_NAME = "jkbms-cache-v1";
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/manifest.json",
+  "/icons/icon-192x192.png",
+  "/icons/icon-512x512.png",
+  "/mqtt.min.js"
+];
+
+// Install service worker and cache static assets
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// Serve cached content when offline
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+// Update cache when a new service worker is activated
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames
+          .filter((name) => name !== CACHE_NAME)
+          .map((name) => caches.delete(name))
+      );
+    })
+  );
+});
